b00923c2a1680883a301285e282b66408a6062ed
ranerijosh <43115830+ranerijosh@users.noreply.github.com>
2021-02-09 12:27:26 -0500

    cleanup

---
+++ Models/DataTransfer/PlayDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Models.DataTransfer
+{
+    public class PlayDto
+    {
+        public int PlayID { get; set; }
+        public int PlaybookID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public byte[] DrawnPlay { get; set; }
+        public string ImageString { get; set; }
+    }
+}
---
+++ Models/Playbook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Playbook
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DisplayName("Playbook ID")]
+        public int PlaybookID { get; set; }
+        [DisplayName("Team ID")]
+        [ForeignKey("TeamID")]
+        public int TeamID { get; set; }
+    }
+}
