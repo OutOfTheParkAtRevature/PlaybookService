503856ea04775d701712baa1f1b4b2755f3833c2
ranerijosh <43115830+ranerijosh@users.noreply.github.com>
2021-02-25 12:12:10 -0500

    Update PlaybookController.cs

--- PlaybookService/Controllers/PlaybookController.cs
+++ PlaybookService/Controllers/PlaybookController.cs
@@ -14,7 +14,6 @@ namespace PlaybookService
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize(Roles = "Admin, League Manager, Head Coach, Assistant Coach, Parent, Player")]
     public class PlaybookController : ControllerBase
     {
         private readonly Logic _logic;
@@ -25,14 +24,12 @@ namespace PlaybookService
             _logger = logger;
         }
         [HttpGet]
-        [Authorize]
         public async Task<IEnumerable<Playbook>> GetPlaybooks()
         {
             return await _logic.GetPlaybooks();
         }
 
         [HttpGet("{id}")]
-        [Authorize]
         public async Task<ActionResult<Playbook>> GetPlaybook(string teamId,string name)
         {
             return await _logic.GetPlaybookById(Guid.Parse(teamId));
@@ -43,43 +40,36 @@ namespace PlaybookService
         //    return await _logic.GetPlaybookById(Guid.Parse(teamId));
         //}
         [HttpGet("plays")]
-        [Authorize]
         public async Task<IEnumerable<PlayDto>> GetPlays()
         {
             return await _logic.GetPlays();
         }
         [HttpGet("plays/{id}")]
-        [Authorize]
         public async Task<ActionResult<PlayDto>> GetPlayDto(string teamId)
         {
             return await _logic.GetPlayDto(Guid.Parse(teamId));
         }
         [HttpPost]
-        [Authorize]
         public async Task<ActionResult<Playbook>> CreatePlaybook(string teamId, string name)
         {
             return await _logic.CreatePlaybook(Guid.Parse(teamId),name);
         }
         [HttpPost("plays")]
-        [Authorize]
         public async Task<ActionResult<Play>> CreatePlay(PlayDto createPlay)
         {
             return await _logic.CreatePlay(createPlay);
         }
         [HttpPut("plays/edit/{id}")]
-        [Authorize]
         public async Task<ActionResult<Play>> EditPlay(string PlayID, PlayDto createPlay)
         {
             return await _logic.EditPlay(Guid.Parse(PlayID), createPlay);
         }
         [HttpDelete("/delete/{id}")]
-        [Authorize]
         public async Task<ActionResult<Playbook>> DeletePlaybook(string id )
         {
             return await _logic.DeletePlaybook(Guid.Parse(id));
         }
         [HttpDelete("plays/delete/{id}")]
-        [Authorize]
         public async Task<ActionResult<Play>> DeletePlay(string id)
         {
             return await _logic.DeletePlay(Guid.Parse(id));
