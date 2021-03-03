3dc805b525b1aad89d5abf52a455b972c3360232
ranerijosh <43115830+ranerijosh@users.noreply.github.com>
2021-03-02 19:20:16 -0500

    .

---
+++ Repo/Migrations/20210302234831_playbookmigration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repository.Migrations
+{
+    public partial class playbookmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Playbooks",
+                columns: table => new
+                {
+                    Playbookid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TeamID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    InDev = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Playbooks", x => x.Playbookid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Plays",
+                columns: table => new
+                {
+                    PlayID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PlaybookId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DrawnPlay = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plays", x => x.PlayID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Playbooks");
+
+            migrationBuilder.DropTable(
+                name: "Plays");
+        }
+    }
+}
